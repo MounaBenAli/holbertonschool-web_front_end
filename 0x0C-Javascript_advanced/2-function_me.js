a05be4882c41ddab2bed102148b36eca5a0ec328
MounaBenAli <benalimouna1986@gmail.com>
2022-09-02 11:10:22 +0200

    Closure

---
+++ 0x0C-Javascript_advanced/2-function_me.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+//closure function
+
+ function welcomeMessage(fullName) {
+    return function () {
+        alert('Welcome '+ fullName );
+    }
+ }
+const guillaume = welcomeMessage('Guillaume');
+const alex = welcomeMessage('Alex');
+const fred = welcomeMessage('Fred');
