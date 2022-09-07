bce5fd00a28279dd591c9ea8aa10e8a17b13b54c
MounaBenAli <benalimouna1986@gmail.com>
2022-09-07 17:14:54 +0200

    Binding

---
+++ 0x0C-Javascript_advanced/12-room_area.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+//Binding
+
+const roomDimensions = {
+    width : 50,
+    length : 100,
+    getArea: function() {
+        return (this.width * this.length);
+    }
+};
+
+const boundGetArea = roomDimensions.getArea.bind(roomDimensions);
+console.log(boundGetArea());
